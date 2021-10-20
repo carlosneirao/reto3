71d65bfe69f74c48b2f166f39e23007076f71d7c
Carlos Neira <carlos.neira.mt@correo.usa.edu.co>
2021-10-20 12:25:14 -0500

    first commit

---
+++ src/main/java/FINCAS/Retos3y4/Retos3y4Application.java
@@ -0,0 +1,15 @@
+
+
+package FINCAS.Retos3y4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Retos3y4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Retos3y4Application.class, args);
+	}
+
+}
---
+++ src/test/java/FINCAS/Retos3y4/Retos3y4ApplicationTests.java
@@ -0,0 +1,13 @@
+package FINCAS.Retos3y4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Retos3y4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
